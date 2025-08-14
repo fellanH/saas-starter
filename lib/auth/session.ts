2bcba52c2158b0c1472aec1397ecdd9b845c2247
fellanH <fehellstrom@gmail.com>
2025-08-14 12:11:59 +0200

    build

--- lib/auth/session.ts
+++ lib/auth/session.ts
@@ -1,9 +1,8 @@
-import { compare, hash } from 'bcryptjs';
-import { SignJWT, jwtVerify } from 'jose';
-import { cookies } from 'next/headers';
-import { NewUser } from '@/lib/db/schema';
+import { compare, hash } from "bcryptjs";
+import { cookies } from "next/headers";
+import { NewUser } from "@/lib/db/schema";
+import { signToken, verifyToken } from "./jwt";
 
-const key = new TextEncoder().encode(process.env.AUTH_SECRET);
 const SALT_ROUNDS = 10;
 
 export async function hashPassword(password: string) {
@@ -22,23 +21,8 @@ type SessionData = {
   expires: string;
 };
 
-export async function signToken(payload: SessionData) {
-  return await new SignJWT(payload)
-    .setProtectedHeader({ alg: 'HS256' })
-    .setIssuedAt()
-    .setExpirationTime('1 day from now')
-    .sign(key);
-}
-
-export async function verifyToken(input: string) {
-  const { payload } = await jwtVerify(input, key, {
-    algorithms: ['HS256'],
-  });
-  return payload as SessionData;
-}
-
 export async function getSession() {
-  const session = (await cookies()).get('session')?.value;
+  const session = (await cookies()).get("session")?.value;
   if (!session) return null;
   return await verifyToken(session);
 }
@@ -50,10 +34,10 @@ export async function setSession(user: NewUser) {
     expires: expiresInOneDay.toISOString(),
   };
   const encryptedSession = await signToken(session);
-  (await cookies()).set('session', encryptedSession, {
+  (await cookies()).set("session", encryptedSession, {
     expires: expiresInOneDay,
     httpOnly: true,
     secure: true,
-    sameSite: 'lax',
+    sameSite: "lax",
   });
 }
